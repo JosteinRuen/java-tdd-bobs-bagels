11a61e74d06617ebcad335e277253dd79257f9e0
Jostein Ruen <josteinruen@gmail.com>
2024-08-14 13:06:00 +0200

    Tests written

--- src/test/java/com/booleanuk/core/BasketTest.java
+++ src/test/java/com/booleanuk/core/BasketTest.java
@@ -3,6 +3,26 @@ package com.booleanuk.core;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+
 class BasketTest {
+
+    @Test
+    public void addBagelTest(){
+        Basket basket = new Basket();
+        Assertions.assertTrue(basket.addBagel("Gluten Bagel"));
+    }
+
+    @Test
+    public void removeBagelTest(){
+        Basket basket = new Basket();
+        Assertions.assertTrue(basket.removeBagel("Gluten Bagel"));
+    }
+
+    @Test
+    public void changeCapacity(){
+        Basket basket = new Basket();
+        Assertions.assertEquals(basket.getSize(),basket.changeCapacity());
+    }
 
 }
